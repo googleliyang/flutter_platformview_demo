28777a47cb1b18c101752fa846ef674b7d4ed57d
necolas <necolas@gmail.com>
2019-05-28 01:09:55 +0800

    -

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,38 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    let viewFactory = FluffViewFactory()
+    
+    registrar(forPlugin: "kitty")
+        .register(viewFactory, withId: "FluffView")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+public class FluffView : NSObject, FlutterPlatformView {
+    
+    let frame: CGRect
+    let viewId: Int64
+    
+    init(_ frame: CGRect, viewId: Int64, args: Any?) {
+        self.frame = frame
+        self.viewId = viewId
+    }
+    
+    public func view() -> UIView {
+        return UISlider(frame: frame)
+    }
+}
+
+
+public class FluffViewFactory : NSObject, FlutterPlatformViewFactory {
+    
+    public func create(withFrame frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?) -> FlutterPlatformView {
+        return FluffView(frame, viewId: viewId, args: args)
+    }
+    
+    
 }
